4b825de190f27aac06c1f5c21399140290020766
ELISENWOLF <aravindprakash2001@gmail.com>
2023-02-14 17:28:59 +0530

    artomic-file-created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Pagination from './Components/HomeComponents/Pagination';
+import CalltoActionSection from './Components/HomeComponents/CalltoActionSection';
+import Contactinfo from './Components/HomeComponents/Contactinfo';
+import Rating from './Components/HomeComponents/Rating';
+import ShopSection from './Components/HomeComponents/ShopSection';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      Test
+    </div>
+  );
+}
+
+export default App;
