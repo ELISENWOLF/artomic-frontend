1804cdf9402a2b641107229a8ee0f2eb3d4ee7c4
ELISENWOLF <aravindprakash2001@gmail.com>
2023-02-20 14:32:57 +0530

    pages-created

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
-import './App.css';
-import './responsive.css'
-import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomeScreen from './Screens/HomeScreen';
-import SingleProduct from './Screens/SingleProduct';
-import Login from './Screens/Login';
-import Register from './Screens/Register';
-import ProfileScreen from './Screens/ProfileScreen';
-import CartScreen from './Screens/CartScreen';
-import ShippingScreen from './Screens/ShippingScreen';
-import PaymentScreen from './Screens/PaymentScreen';
-import PlacedOrderScreen from './Screens/PlacedOrderScreen';
-import OrderScreen from './Screens/OrderScreen';
-import NotFound from './Screens/NotFound';
-
+import "./App.css";
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return (
-   <Router>
-    <Switch>
-      <Route path='/' component={HomeScreen} />
-      <Route path= '/products/:id' component={SingleProduct} />   
-      <Route path='/login' component={Login} />
-      <Route path='/register' component={Register} />
-      <Route path='/profile' component={ProfileScreen} />
-      <Route path='/cart/:id?' component={CartScreen} />
-      <Route path='/shipping' component={ShippingScreen} />
-      <Route path='/payment' component={PaymentScreen} />
-      <Route path='/placeorder' component={PlacedOrderScreen} />
-      <Route path='/order' component={OrderScreen} />
-      <Route path='*' component={NotFound} />
-    </Switch>
-   </Router>
-  );
+  return <Layout />;
 }
 
 export default App;
-
-// Time: 20:33
+//22.34
