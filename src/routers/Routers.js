2fee26a2803887af3a9c38c80704b0e2190e48c0
ELISENWOLF <aravindprakash2001@gmail.com>
2023-02-23 13:42:06 +0530

    hero-img-animted-1

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import Shop from '../pages/Shop'
@@ -12,7 +12,6 @@ import Signup from '../pages/Signup'
 const Routers = () => {
   return (
     <Routes>
-      {/* <Route path='/' element={<Navigate to='home' />} /> */}
       <Route path='/' element={<Home />} />
       <Route path='shop' element={<Shop />} />
       <Route path='cart' element={<Cart />} />
