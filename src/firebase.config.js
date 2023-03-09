aea0520c5f7b5afd53e383b5f34d99ad66e1e019
ELISENWOLF <aravindprakash2001@gmail.com>
2023-03-09 23:02:18 +0530

    firbase-config1

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,11 +5,11 @@ import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyAvWEf3Kk_gly8tr_8jzmTZAOg4l-ixN0o",
-  authDomain: process.env.REACT_APP_FIREBASE_DOMEAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  authDomain: "artomic-ecommerce.firebaseapp.com",
+  projectId: "artomic-ecommerce",
+  storageBucket: "artomic-ecommerce.appspot.com",
+  messagingSenderId: "283430494883",
+  appId: "1:283430494883:web:5798d2a5a6c02ba9355516"
 };
 
 // Initialize Firebase
